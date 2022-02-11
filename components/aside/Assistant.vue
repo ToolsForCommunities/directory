03dc9ecb2490951b0b1c02f46f21e893289e4c8e
Ivo <10926362+ivoriginal@users.noreply.github.com>
2022-02-11 12:12:16 +0100

    Update Assistant.vue

--- components/aside/Assistant.vue
+++ components/aside/Assistant.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <v-toolbar
+    <!-- <v-toolbar
       flat
       color="white"
     >
       <v-toolbar-title class="px-4 font-weight-bold fill-width">
         Assistant
       </v-toolbar-title>
-    </v-toolbar>
+    </v-toolbar> -->
 
-    <div class="mx-4 my-2">
+    <div class="mx-4 my-4">
       <AssistantCard>
         <header class="pa-4">
-          <h2>Getting</h2>
+          <h2>Getting started</h2>
         </header>
 
         <v-divider />
@@ -20,7 +20,7 @@
         <section class="pa-4">
           <h3>Finish your profile</h3>
           <p class="mb-0">
-            Help other find your
+            Help other find you
           </p>
         </section>
 
@@ -29,32 +29,9 @@
         <section class="pa-4">
           <h3>Connect your Slack acoount</h3>
           <p class="mb-0">
-            Are you new to the community and want to connect with other startups? Are you looking for other members with a specific expertise to help you grow your company?
+            Are you looking for other members with a specific expertise to help you grow your company?
           </p>
-        </section>
-      </AssistantCard>
-
-      <AssistantCard class="mt-4">
-        <header class="pa-4">
-          <h2>We care</h2>
-        </header>
-
-        <v-divider />
-
-        <section class="pa-4">
-          <h3>Finish your profile</h3>
-          <p class="mb-0">
-            Are you new to the community and want to connect with other startups? Are you looking for other members with a specific expertise to help you grow your company?
-          </p>
-        </section>
-
-        <v-divider />
-
-        <section class="pa-4">
-          <h3>Connect your Slack acoount</h3>
-          <p class="mb-0">
-            Are you new to the community and want to connect with other startups? Are you looking for other members with a specific expertise to help you grow your company?
-          </p>
+          <!-- <CTButton text class="pa-0 ma-0">Start</CTButton> -->
         </section>
       </AssistantCard>
     </div>
