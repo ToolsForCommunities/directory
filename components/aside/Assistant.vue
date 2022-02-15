5291e8259967c94d54d0ef4575e031cf924a31ef
Ivo <10926362+ivoriginal@users.noreply.github.com>
2022-02-15 14:04:01 +0100

    Update Assistant.vue

--- components/aside/Assistant.vue
+++ components/aside/Assistant.vue
@@ -10,9 +10,33 @@
     </v-toolbar> -->
 
     <div class="mx-4 my-4">
-      <AssistantCard>
+      <AssistantCard class="mb-4">
         <header class="pa-4">
           <h2>Getting started</h2>
+        </header>
+
+        <v-divider />
+
+        <section class="pa-4">
+          <h3>Finish your profile</h3>
+          <p class="mb-0">
+            Help other find you
+          </p>
+        </section>
+
+        <v-divider />
+
+        <section class="pa-4">
+          <h3>Connect your Slack account</h3>
+          <p class="mb-0">
+            Are you looking for other members with a specific expertise to help you grow your company?
+          </p>
+          <!-- <CTButton text class="pa-0 ma-0">Start</CTButton> -->
+        </section>
+      </AssistantCard>
+      <AssistantCard>
+        <header class="pa-4">
+          <h2>Tips & tricks</h2>
         </header>
 
         <v-divider />
