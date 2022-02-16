5781388ddd46e5adf5b3f76bdfcc2d9c6c04671b
Ivo <10926362+ivoriginal@users.noreply.github.com>
2022-02-16 14:43:56 +0100

    #13
    is this the GDPR compliant interaction @CodingCarlos ?

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,52 @@
       <SkeletonLayout v-if="loading" />
     </transition>
 
+    <!-- Alert/cookies -->
+    <v-alert
+      text
+      color="info"
+      class="mb-0"
+    >
+      <v-row
+        align="center"
+        no-gutters
+        class="justify-space-between"
+      >
+        <v-col class="col-12 col-md-6 mb-4 mb-lg-0">
+          <!-- <h3 class="text-h5">
+            Take a minute to review your privacy settings
+          </h3> -->
+          <span>Community Tools tracks some of the actions users take inside our products to understand how they use them with a technology called "cookies". You can configure how these cookies work, accept or reject all.</span>
+        </v-col>
+        <!-- <v-spacer></v-spacer> -->
+        <v-col class="shrink">
+          <div class="d-flex flex-row">
+            <CTButton
+              color="info"
+              outlined
+              class="mr-2"
+            >
+              Reject all
+            </CTButton>
+            <CTButton
+              color="info"
+              outlined
+              class="mr-2"
+            >
+              Configure
+            </CTButton>
+            <CTButton
+              color="info"
+              outlined
+            >
+              Accept all
+            </CTButton>
+          </div>
+        </v-col>
+      </v-row>
+    </v-alert>
+    <!-- Alert/cookies -->
+
     <CTLayoutMain :hide-aside="hideAside">
       <NavigationMain slot="sidebar" />
 
