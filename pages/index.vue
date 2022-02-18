112cd68e869daed946d8feb35cb56f8112216f22
Ivo <10926362+ivoriginal@users.noreply.github.com>
2022-02-18 13:25:32 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,239 @@
     <NavigationMainTopbar
       title="Home"
     />
-    <v-container>
-      lol
-      <HelloWorld />
-      lol
-      <HelloWorld />
-      lol
-      <HelloWorld />
-      <HelloWorld />
+    <v-container class="pa-0">
+      <!-- Alert -->
+      <!-- <v-alert
+        text
+        color="info"
+      >
+        <h3 class="text-h5">
+          Lorem Ipsum
+        </h3>
+        <div>Maecenas nec odio et ante tincidunt tempus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. Curabitur turpis.</div>
+
+        <v-divider
+          class="my-4 info"
+          style="opacity: 0.22"
+        ></v-divider>
+
+        <v-row
+          align="center"
+          no-gutters
+        >
+          <v-col class="grow">
+            Proin magna. Vivamus in erat ut urna cursus vestibulum. Etiam imperdiet imperdiet orci.
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col class="shrink">
+            <v-btn
+              color="info"
+              outlined
+            >
+              Okay
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-alert> -->
+      <!-- /Alert -->
+      <!-- Cards -->
+      <v-card
+        class="pa-4"
+        flat
+      >
+        <div class="flex-sm-column-reverse flex-lg-row flex-no-wrap justify-space-between">
+          <div>
+            <div class="text-overline">
+              Community
+            </div>
+            <v-card-title class="px-0 pt-0">
+              New faces in your community
+            </v-card-title>
+            <v-card-subtitle class="px-0 pr-md-6">
+              Remember your first day of school? Was it hard? You wish you had some friends? Then go and befriend these new members!
+            </v-card-subtitle>
+            <!-- <div class="d-flex" style="width: 100%;"> -->
+            <v-slide-group
+                multiple
+                show-arrows
+              >
+                <v-slide-item>
+                  <div data-v-3b8704c8="" class="col-lg-4 col-6 pa-0">
+                    <a data-v-68c7e3f4="" data-v-3b8704c8="" href="/people?aside=person%2FDXyS4j1H9LvSd22C" class="mx-auto my-1 v-card v-card--flat v-card--link v-sheet v-sheet--outlined theme--light" tabindex="0">
+                      <div data-v-68c7e3f4="" class="v-image v-responsive theme--light">
+                        <div class="v-responsive__sizer" style="padding-bottom: 100%;"></div>
+                        <div class="v-image__image v-image__image--cover" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/y/2/y2VisJqVQNSFuEogEkNoF.png&quot;); background-position: center center;">
+                        </div>
+                        <div class="v-responsive__content">
+                        </div>
+                      </div>
+                      <hr data-v-68c7e3f4="" role="separator" aria-orientation="horizontal" class="v-divider theme--light">
+                      <div data-v-68c7e3f4="">
+                        <div data-v-68c7e3f4="" title="Marta Zaragozá" class="v-card__title pb-0 pt-2 px-4 text-truncate d-block">
+                          Marta Zaragozá
+                        </div>
+                        <div data-v-68c7e3f4="" class="v-card__text">
+                          <div data-v-68c7e3f4="" class="subtitle-1 text-truncate">
+                            <span data-v-68c7e3f4="">
+                              CEO
+                            </span>
+                            <br data-v-68c7e3f4="">
+                            <span data-v-68c7e3f4="">
+                              Declarando
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </v-slide-item>
+
+                <v-slide-item>
+                  <div data-v-3b8704c8="" class="col-lg-4 col-6 mx-1">
+                <a data-v-68c7e3f4="" data-v-3b8704c8="" href="/people?aside=person%2FDXyS4j1H9LvSd22C" class="mx-auto my-1 v-card v-card--flat v-card--link v-sheet v-sheet--outlined theme--light" tabindex="0">
+                  <div data-v-68c7e3f4="" class="v-image v-responsive theme--light">
+                    <div class="v-responsive__sizer" style="padding-bottom: 100%;"></div>
+                    <div class="v-image__image v-image__image--cover" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/y/2/y2VisJqVQNSFuEogEkNoF.png&quot;); background-position: center center;">
+                    </div>
+                    <div class="v-responsive__content">
+                    </div>
+                  </div>
+                  <hr data-v-68c7e3f4="" role="separator" aria-orientation="horizontal" class="v-divider theme--light">
+                  <div data-v-68c7e3f4="">
+                    <div data-v-68c7e3f4="" title="Marta Zaragozá" class="v-card__title pb-0 pt-2 px-4 text-truncate d-block">
+                      Marta Zaragozá
+                    </div>
+                    <div data-v-68c7e3f4="" class="v-card__text">
+                      <div data-v-68c7e3f4="" class="subtitle-1 text-truncate">
+                        <span data-v-68c7e3f4="">
+                          CEO
+                        </span>
+                        <br data-v-68c7e3f4="">
+                        <span data-v-68c7e3f4="">
+                          Declarando
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                  </div>
+                </v-slide-item>
+                <v-slide-item>
+                  <div data-v-3b8704c8="" class="col-lg-4 col-6 pa-0">
+                    <a data-v-68c7e3f4="" data-v-3b8704c8="" href="/people?aside=person%2FDXyS4j1H9LvSd22C" class="mx-auto my-1 v-card v-card--flat v-card--link v-sheet v-sheet--outlined theme--light" tabindex="0">
+                      <div data-v-68c7e3f4="" class="v-image v-responsive theme--light">
+                        <div class="v-responsive__sizer" style="padding-bottom: 100%;"></div>
+                        <div class="v-image__image v-image__image--cover" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/y/2/y2VisJqVQNSFuEogEkNoF.png&quot;); background-position: center center;">
+                        </div>
+                        <div class="v-responsive__content">
+                        </div>
+                      </div>
+                      <hr data-v-68c7e3f4="" role="separator" aria-orientation="horizontal" class="v-divider theme--light">
+                      <div data-v-68c7e3f4="">
+                        <div data-v-68c7e3f4="" title="Marta Zaragozá" class="v-card__title pb-0 pt-2 px-4 text-truncate d-block">
+                          Marta Zaragozá
+                        </div>
+                        <div data-v-68c7e3f4="" class="v-card__text">
+                          <div data-v-68c7e3f4="" class="subtitle-1 text-truncate">
+                            <span data-v-68c7e3f4="">
+                              CEO
+                            </span>
+                            <br data-v-68c7e3f4="">
+                            <span data-v-68c7e3f4="">
+                              Declarando
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </v-slide-item>
+            </v-slide-group>
+            <!-- </div> -->
+            <!-- <v-card-actions>
+              <CTButton
+                text
+              >
+                Learn more
+              </CTButton>
+            </v-card-actions> -->
+          </div>
+        </div>
+      </v-card>
+      <v-divider></v-divider>
+
+      <v-card
+        class="pa-4 mb-0"
+        flat
+      >
+        <div class="d-flex flex-column-reverse flex-md-row flex-no-wrap justify-space-between">
+          <div>
+            <div class="text-overline">
+              News
+            </div>
+            <v-card-title class="px-0 pt-0">
+              Waze founder visiting the space
+            </v-card-title>
+            <v-card-subtitle class="px-0">
+              Tomorrow at 13:30 Johanna Cohen will visit our Cafe space for a quick fireside chat with the community
+            </v-card-subtitle>
+
+            <!-- <v-card-actions>
+              <CTButton
+                text
+              >
+                Learn more
+              </CTButton>
+            </v-card-actions> -->
+          </div>
+
+          <v-avatar
+            tile
+            size="124"
+            color="grey"
+            class="d-sm-flex ml-md-4 mb-2 mb-md-0"
+          >
+            <v-img src="https://jewishbusinessnews.com/wp-content/uploads/2013/06/uri-levine.jpg"></v-img>
+          </v-avatar>
+        </div>
+      </v-card>
+      <v-divider></v-divider>
+
+      <v-card
+        class="pa-4 mb-0"
+        flat
+      >
+        <div class="d-flex flex-column-reverse flex-md-row flex-no-wrap justify-space-between">
+          <div>
+            <div class="text-overline">
+              News
+            </div>
+            <v-card-title class="px-0 pt-0">
+              Vostok 6 es adquirido por La Ser Podcasts
+            </v-card-title>
+            <v-card-subtitle class="px-0">
+              Tomorrow at 13:30 Johanna Cohen will visit our Cafe space for a quick fireside chat with the community
+            </v-card-subtitle>
+          </div>
+
+          <v-avatar
+            tile
+            size="124"
+            color="grey"
+            class="d-sm-flex ml-md-4 mb-2 mb-md-0"
+          >
+            <v-img src="http://static-1.ivoox.com/canales/5/5/3/3/5533eafd2ae231698df95a8c79764cd8_XXL.jpg"></v-img>
+          </v-avatar>
+        </div>
+      </v-card>
+      <v-divider></v-divider>
+      <!-- /Cards -->
+      <!-- After -->
+      <div class="d-flex justify-center py-4">
+        <span>You're up to date. Go have some <a href="https://www.urbandictionary.com/define.php?term=jomo" target="_blank">JOMO</a>.
+      </span>
+      </div>
+      <!-- /After -->
     </v-container>
   </div>
 </template>
@@ -39,6 +264,9 @@ export default {
 </script>
 
 <style scoped>
+.v-card {
+  margin-bottom: 8px;
+}
 .ct-container {
   height: 100vh;
   max-height: 100vh;
@@ -47,4 +275,11 @@ export default {
   .ct-container__has-topbar {
     padding-top: 64px;
   }
+
+@media screen  and (max-width: 959px) {
+  .v-avatar {
+    width: 100% !important;
+    height: 56vw !important;
+  }
+}
 </style>
