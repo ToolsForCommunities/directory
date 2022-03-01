100fed1071c1ab2c35fecac0ae4a9418151d1f6d
CodingCarlos <hola@codingcarlos.com>
2022-03-01 11:57:56 +0100

    Fix deploy error

--- store/helpers/api.js
+++ store/helpers/api.js
@@ -3,12 +3,14 @@ const data = {
   setUrl
 }
 
-function setUrl (url, env) {
-  data.url = '/api'
+// function setUrl (url, env) {
+function setUrl (url) {
+  data.url = url
 
-  if (env !== 'production' && url) {
-    data.url = url
-  }
+  // data.url = '/api'
+  // if (env !== 'production' && url) {
+  //   data.url = url
+  // }
 }
 
 export default data
--- store/index.js
+++ store/index.js
@@ -3,9 +3,17 @@ import api from '@/store/helpers/api'
 /**
  *  Initial app load
  */
-function init ({ dispatch }, ctx) {
+function init ({ dispatch }, { $config, req }) {
   // Prepare API URL
-  api.setUrl(ctx.$config.VUE_APP_API_URL, ctx.$config.NODE_ENV)
+  // api.setUrl($config.VUE_APP_API_URL, $config.NODE_ENV)
+  let url = process.server ? req.headers.host : window.location.host
+  url = `https://${url}/api`
+
+  if ($config.VUE_APP_API_URL) {
+    url = $config.VUE_APP_API_URL
+  }
+  // Set the API base url
+  api.setUrl(url)
 
   // Prepare app settings
   dispatch('config/get', { global: true })
