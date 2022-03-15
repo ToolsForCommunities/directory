d171641656a5d00d8d83f6fa0ebf79da3777181a
CodingCarlos <hola@codingcarlos.com>
2022-03-15 15:28:52 +0100

    Add dropdown

--- components/person/PersonConnect.vue
+++ components/person/PersonConnect.vue
@@ -1,9 +1,11 @@
 <template>
   <div v-if="active.length > 0">
-    <div class="text-center">
-      <!-- Email connect -->
-      <!-- ToDo: Hide if email connect is not enabled -->
-      <nuxt-link :to="`?aside=email/${item.id}`">
+    <div class="text-center my-4">
+      <!-- Email connect (hidden if not enabled) -->
+      <nuxt-link
+        v-if="$settings.emailConnect"
+        :to="`?aside=email/${item.id}`"
+      >
         <CTButton
           primary
           class="my-4 mx-1"
@@ -22,14 +24,19 @@
       >
         <CTButton
           primary
-          class="my-4 mx-1"
+          class="mx-1"
           @click="$store.dispatch('track/event', { action: 'connect', category: 'person_detail', label: platform.field })"
         >
           {{ platform.name }}
         </CTButton>
       </a>
 
-      <!-- ToDo: Dropdown with all the other options -->
+      <!-- Dropdown with all the other options -->
+      <PersonConnectDropdown
+        v-if="dropdown && dropdown.length"
+        :options="dropdown"
+        @click="$store.dispatch('track/event', { action: 'connect_dropdown', category: 'person_detail' })"
+      />
     </div>
   </div>
 </template>
@@ -85,22 +92,27 @@ export default {
     active () {
       return this.platforms.filter(platform => !!this.item[platform.field])
     },
+    itemsPreDropdown () {
+      let itemsPreDropdown = 3
+      if (this.$settings.emailConnect) {
+        itemsPreDropdown -= 1
+      }
+
+      return itemsPreDropdown
+    },
     platformList () {
-      if (this.active.length <= 3) {
+      if (this.active.length <= this.itemsPreDropdown) {
         return this.active
       }
 
-      return [
-        this.active[0],
-        this.active[1]
-      ]
+      return this.active.slice(0, this.itemsPreDropdown - 1)
     },
     dropdown () {
-      if (this.active.length <= 3) {
+      if (this.active.length <= this.itemsPreDropdown) {
         return null
       }
 
-      return this.active.slice(2)
+      return this.active.slice(this.itemsPreDropdown - 1)
     }
   }
 }
